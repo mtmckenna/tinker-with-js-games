0a141048a3187e35c11738db0b30bc269c4f3f4c
Matt McKenna <matt@mtmckenna.com>
2019-02-10 18:33:11 -0800

    Don't rely on S3 to redirect

--- index.js
+++ index.js
@@ -2,32 +2,34 @@ import "./index.css";
 
 const GET_UPLOAD_PATH_URL = "https://g5f83oj990.execute-api.us-east-1.amazonaws.com/prod/upload-url";
 const URL_PREFIX = "https://tjsg.mtmckenna.com";
+let uploadCreds = null;
 
 window.addEventListener("load", async function () {
-  const getUrlResponse = await getUploadUrl();
-  const responseData = await getUrlResponse.json();
-  const json = JSON.parse(responseData.body);
-  const { fields, url } = json;
+  uploadCreds = await getUpdatedUploadCreds();
+  const { fields, url } = uploadCreds;
   const redirectUrl = `${URL_PREFIX}/${fields['key'].split("/")[0]}`;
   const form = document.getElementById("upload-form");
+  const formData = new FormData();
 
-  form.setAttribute("action", url);
-  form.prepend(createHiddenInput("success_action_redirect", redirectUrl));
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    formData.append("acl", "public-read");
+    formData.append("Content-Type", "text/html");
+    Object.keys(fields).forEach((fieldName) => formData.append(fieldName, fields[fieldName]));
+    formData.append("file", document.getElementById("file").value);
 
-  Object.keys(fields).forEach((fieldName) => {
-    const input = createHiddenInput(fieldName, fields[fieldName]);
-    form.prepend(input);
+    await fetch(url, { body: formData, method: "POST" });
+    uploadCreds = await getUpdatedUploadCreds();
+    window.location.href = redirectUrl;
   });
 });
 
-async function getUploadUrl() {
-  return await fetch(GET_UPLOAD_PATH_URL);
+async function getUpdatedUploadCreds() {
+  const getUrlResponse = await getUploadUrl();
+  const responseData = await getUrlResponse.json();
+  return JSON.parse(responseData.body);
 }
 
-function createHiddenInput(name, value) {
-  const input = document.createElement("input");
-  input.setAttribute("type", "hidden");
-  input.setAttribute("name", name);
-  input.setAttribute("value", value);
-  return input;
+async function getUploadUrl() {
+  return await fetch(GET_UPLOAD_PATH_URL);
 }
--- lambdas/get-upload-url.js
+++ lambdas/get-upload-url.js
@@ -4,32 +4,31 @@ const util = require("util");
 
 const { S3 } = aws;
 
-exports.handler = async(event) => {
-    const s3 = new S3();
-    const s3Path = `z${crypto.randomBytes(16).toString("hex")}`;
-    const redirectUrl = `https://tjsg.mtmckenna.com/${s3Path}`;
-    const params = {
-        Bucket: "tinker-with-js-games-html-uploads",
-        Conditions: [
-            { acl: "public-read" },
-            { "Content-Type": "text/html"},
-            ["content-length-range", 1, 2097152],
-            ["starts-with", "$success_action_redirect", redirectUrl],
-            ],
-        Fields: { key: s3Path }
-    };
+exports.handler = async (event) => {
+  const s3 = new S3();
+  const s3Path = `z${crypto.randomBytes(16).toString("hex")}`;
+  const redirectUrl = `https://tjsg.mtmckenna.com/${s3Path}`;
+  const params = {
+    Bucket: "tinker-with-js-games-html-uploads",
+    Conditions: [
+      { acl: "public-read" },
+      { "Content-Type": "text/html" },
+      ["content-length-range", 1, 2097152],
+    ],
+    Fields: { key: s3Path }
+  };
 
-    const createPresignedPost = util.promisify(s3.createPresignedPost).bind(s3);
-    const postResponse = await createPresignedPost(params);
+  const createPresignedPost = util.promisify(s3.createPresignedPost).bind(s3);
+  const postResponse = await createPresignedPost(params);
 
-    const response = {
-        statusCode: 200,
-        headers: {
-            "Access-Control-Allow-Origin": "https://tjsg.mtmckenna.com",
-            "Access-Control-Allow-Credentials": true,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(postResponse),
-    };
-    return response;
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postResponse),
+  };
+  return response;
 };
